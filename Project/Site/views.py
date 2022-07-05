f4a89764eeca2a049628ab4c18b037075d3b35fc
claudiu28 <claudiudraghita28@gmail.com>
2022-07-05 15:48:54 +0300

    commit

--- Project/Site/views.py
+++ Project/Site/views.py
@@ -1,8 +1,8 @@
 from re import T
 from django.shortcuts import render,redirect
 from django.contrib.auth import logout,login,authenticate
-from .models import Profile,PostLessonTeacher
-from .forms import  Register
+from .models import Question,Profile,PostLessonTeacher
+from .forms import  Register,QuestionsQuiz
 from django.contrib.auth.models import User
 from itertools import chain
 from django.contrib.auth.decorators import login_required
@@ -75,20 +75,137 @@ def create_lesson(request):
 		return redirect('home')
 	return render(request,'utilites/lessons.html',{'UserProfile':UserProfile})
 
-def files(request):
-	return render(request,'media/files/sdac.pdf',{})
+
 def displaylessons(request):
 	posts2 = PostLessonTeacher.objects.all()
 	return render(request,'utilites/displaylessons.html',{'posts2':posts2})
 
 
-def create_quiz(request):
+def index(request):
+    quiz = Quiz.objects.all()
+    para = {'quiz' : quiz}
+    return render(request, "index.html", para)
 
-	return render(request,'utilites/uploadlesson.html',{})
+@login_required(login_url = '/login')
+def quiz(request, myid):
+    quiz = Quiz.objects.get(id=myid)
+    return render(request, "quiz.html", {'quiz':quiz})
 
-def displayquiz(request):
-	pass
+def quiz_data_view(request, myid):
+    quiz = Quiz.objects.get(id=myid)
+    questions = []
+    for q in quiz.get_questions():
+        answers = []
+        for a in q.get_answers():
+            answers.append(a.content)
+        questions.append({str(q): answers})
+    return JsonResponse({
+        'data': questions,
+        'time': quiz.time,
+    })
 
+
+def save_quiz_view(request, myid):
+    if request.is_ajax():
+        questions = []
+        data = request.POST
+        data_ = dict(data.lists())
+
+        data_.pop('csrfmiddlewaretoken')
+
+        for k in data_.keys():
+            print('key: ', k)
+            question = Question.objects.get(content=k)
+            questions.append(question)
+
+        user = request.user
+        quiz = Quiz.objects.get(id=myid)
+
+        score = 0
+        marks = []
+        correct_answer = None
+
+        for q in questions:
+            a_selected = request.POST.get(q.content)
+
+            if a_selected != "":
+                question_answers = Answer.objects.filter(question=q)
+                for a in question_answers:
+                    if a_selected == a.content:
+                        if a.correct:
+                            score += 1
+                            correct_answer = a.content
+                    else:
+                        if a.correct:
+                            correct_answer = a.content
+
+                marks.append({str(q): {'correct_answer': correct_answer, 'answered': a_selected}})
+            else:
+                marks.append({str(q): 'not answered'})
+     
+        Marks_Of_User.objects.create(quiz=quiz, user=user, score=score)
+        
+        return JsonResponse({'passed': True, 'score': score, 'marks': marks})
+    
+
+
+
+
+def add_quiz(request):
+    if request.method=="POST":
+        form = QuizForm(data=request.POST)
+        if form.is_valid():
+            quiz = form.save(commit=False)
+            quiz.save()
+            obj = form.instance
+            return render(request, "add_quiz.html", {'obj':obj})
+    else:
+        form=QuizForm()
+    return render(request, "add_quiz.html", {'form':form})
+
+def add_question(request):
+    questions = Question.objects.all()
+    questions = Question.objects.filter().order_by('-id')
+    if request.method=="POST":
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, "add_question.html")
+    else:
+        form=QuestionForm()
+    return render(request, "add_question.html", {'form':form, 'questions':questions})
+
+def delete_question(request, myid):
+    question = Question.objects.get(id=myid)
+    if request.method == "POST":
+        question.delete()
+        return redirect('/add_question')
+    return render(request, "delete_question.html", {'question':question})
+
+
+def add_options(request, myid):
+    question = Question.objects.get(id=myid)
+    QuestionFormSet = inlineformset_factory(Question, Answer, fields=('content','correct', 'question'), extra=4)
+    if request.method=="POST":
+        formset = QuestionFormSet(request.POST, instance=question)
+        if formset.is_valid():
+            formset.save()
+            alert = True
+            return render(request, "add_options.html", {'alert':alert})
+    else:
+        formset=QuestionFormSet(instance=question)
+    return render(request, "add_options.html", {'formset':formset, 'question':question})
+
+def results(request):
+    marks = Marks_Of_User.objects.all()
+    return render(request, "results.html", {'marks':marks})
+
+def delete_result(request, myid):
+    marks = Marks_Of_User.objects.get(id=myid)
+    if request.method == "POST":
+        marks.delete()
+        return redirect('/results')
+    return render(request, "delete_result.html", {'marks':marks})
 
 
 def searchteacher(request):
@@ -108,6 +225,19 @@ def searchteacher(request):
 	return render(request, 'search.html', {'user_profile': user_profile, 'username_profile_list': usernameprofilelist})
 
 
+def profile(request, pk):
+    user_object = User.objects.get(username=pk)
+    user_profile = Profile.objects.get(user=user_object)
+    user_posts = PostLessonTeacher.objects.filter(user=pk)
+    user_post_length = len(user_posts)
+    user = pk
+    context = {
+        'user_object': user_object,
+        'user_profile': user_profile,
+        'user_posts': user_posts,
+        'user_post_length': user_post_length,
+    }
+    return render(request, 'profile.html', context)
 
 
 
